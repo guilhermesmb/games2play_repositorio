2189e78f52c9bd5e9dd42cf93b5b43f2df221b54
Arthur Tortoreli <66446301+ArthurTortoreli@users.noreply.github.com>
2023-05-16 22:36:19 -0300

    Add files via upload

--- crud.js
+++ crud.js
@@ -3,11 +3,14 @@ document.querySelector("#salvar").addEventListener("click", cadastrar);
 let lista_tarefas = [];
 
 window.addEventListener("load", () => {
-    const tarefas = JSON.parse(localStorage.getItem("lista_tarefa"));
+    lista_tarefas = JSON.parse(localStorage.getItem("lista_tarefa")) || [];
 
-    tarefas.forEach(tarefa => {
-        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa);
-    })
+    atualizar();
+})
+
+document.querySelector("#pendentes").addEventListener("click", () => {
+    lista_tarefas = lista_tarefas.filter(tarefa => tarefa.concluida == false)
+    atualizar()
 })
 
 function cadastrar() {
@@ -18,10 +21,12 @@ function cadastrar() {
     let categoria = document.querySelector("#categoria").value;
 
     const tarefa = {
+        id: Date.now(),
         titulo: titulo,
         descricao: descricao,
         pontos: pontos,
         categoria: categoria,
+        concluida: false
     }
 
     if (tarefa.titulo.length == 0) {
@@ -36,16 +41,41 @@ function cadastrar() {
     document.querySelector("#titulo").value = "";
     document.querySelector("#descricao").value = "";
 
-    localStorage.setItem("lista_tarefa", JSON.stringify(lista_tarefas));
+    salvar();
 
     modal.hide();
 }
 
-function apagar(botao) {
-    botao.parentNode.parentNode.parentNode.remove();
+function atualizar() {
+    document.querySelector("#tarefas").innerHTML = "";
+
+    lista_tarefas.forEach((tarefa) => {
+        document.querySelector("#tarefas").innerHTML += gerarCard(tarefa);
+    })
+}
+
+function salvar() {
+    localStorage.setItem("lista_tarefa", JSON.stringify(lista_tarefas));
+}
+
+function apagar(id) {
+    lista_tarefas = lista_tarefas.filter(tarefa => tarefa.id != id)
+
+    salvar();
+    atualizar();
+}
+
+function concluir(id) {
+    let tarefa_encontrada = lista_tarefas.find(tarefa => tarefa.id == id);
+
+    tarefa_encontrada.concluida = true;
+
+    salvar();
+    atualizar();
 }
 
 function gerarCard(tarefa) {
+    const disabled = (tarefa.concluida) ? "disabled" : "";
     return `
         <div class="col-lg-3 col-md-6 col-12">
             <div class="card">
@@ -58,10 +88,10 @@ function gerarCard(tarefa) {
                         <span class="badge text-bg-warning">${tarefa.categoria}</span>
                     </p>
                     <p>${tarefa.pontos} pts</p>
-                    <a href="#" class="btn btn-success">
+                    <a href="#" onClick="concluir(${tarefa.id})" class="btn btn-success ${disabled}">
                         <i class="bi bi-check-lg"></i>
                     </a>
-                    <a href="#" onClick="apagar(this)" class="btn btn-danger">
+                    <a href="#" onClick="apagar(${tarefa.id})" class="btn btn-danger">
                         <i class="bi bi-trash"></i>
                     </a>
                 </div>
